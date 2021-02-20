ce213a595efc0171c5cec684f7ad43c4c298c7a8
wackycodes <wackycodes@gmail.com>
2021-02-20 23:04:18 +0530

    Commit No. 20 02 20 : Add ActivityAddNewProduct.java and activity_add_new_product.xml

---
+++ app/src/main/java/com/sunitech/myapp/addnew/ActivityAddNewProduct.java
@@ -0,0 +1,16 @@
+package com.sunitech.myapp.addnew;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.sunitech.myapp.R;
+
+public class ActivityAddNewProduct extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_add_new_product );
+    }
+}
